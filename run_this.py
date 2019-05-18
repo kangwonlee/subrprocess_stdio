380f6f85f503963fd8768cc2060f96c5fa8cd9f6
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-05-18 19:23:23 +0900

    running a python script with input()

--- run_this.py
+++ run_this.py
@@ -1,9 +1,28 @@
+import os
 import subprocess
+import sys
 
 
-def main():
-    pass
+def main(argv):
+    script_filename = argv[0]
+
+    assert os.path.exists(script_filename)
+
+    p = subprocess.Popen([sys.executable, script_filename], 
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding='utf-8',
+    )
+
+    first_output = p.stdout.read(1)
+    print(first_output)
+
+    stdout, stderr = p.communicate(input='a\nb\n')
+
+    print(f"stdout={repr(stdout)}")
+    print(f"stderr={repr(stderr)}")
 
 
 if "__main__" == __name__:
-    main()
+    main(sys.argv[1:])
